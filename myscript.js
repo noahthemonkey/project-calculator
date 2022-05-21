bff7346268d2dfdf115bd8cc93bc4e0d450f0819
Noah Msekni <msekninoah@gmail.com>
2022-05-21 12:40:59 +0200

    Set up files, add divs and containers and some style

---
+++ myscript.js
@@ -0,0 +1,8 @@
+const calculatorContainer = document.querySelector('#calculator-container')
+
+// Container and Divs for addition, subtraction, multiplication and division
+const operationsContainer = document.createElement('div')
+    operationsContainer.setAttribute('id', 'operations-container')
+
+const addDiv = document.createElement('div')
+    addDiv.setAttribute('id', 'addDiv')
