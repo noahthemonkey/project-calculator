6663e1d1fa39c5196846ffa53f257444653df2a8
Noah Msekni <msekninoah@gmail.com>
2022-05-22 14:19:22 +0200

    Add buttons for numbers and functions

--- myscript.js
+++ myscript.js
@@ -1,8 +1,40 @@
 const calculatorContainer = document.querySelector('#calculator-container')
 
-// Container and Divs for addition, subtraction, multiplication and division
-const operationsContainer = document.createElement('div')
-    operationsContainer.setAttribute('id', 'operations-container')
+// Number buttons 
+let numbers = 10;
+for (i = 0; i < numbers; i++) {
+    const numbersContainer = document.getElementById('numbers-container');
+    const calcNumber = document.createElement('button');
+    calcNumber.setAttribute('id', `number${i}`);
+    calcNumber.textContent = `${i}`;
+    calcNumber.addEventListener('click', selectorFunction);
+    numbersContainer.appendChild(calcNumber);
+}
+let selectedNum = '';
+// Selected number
+function selectorFunction(selected) {
+    selected = this.textContent
+    selectedNum += selected
+    console.log(selectedNum)
+}
 
-const addDiv = document.createElement('div')
-    addDiv.setAttribute('id', 'addDiv')
+
+// operator function
+const operationsContainer = document.getElementById('operations-container');
+
+const addButton = document.getElementById('add-button')
+    addButton.addEventListener('click', operator)
+const subButton = document.getElementById('sub-button')
+    subButton.addEventListener('click', operator)
+const multiButton = document.getElementById('multi-button')
+    multiButton.addEventListener('click', operator)
+const diviButton = document.getElementById('divi-button')
+    diviButton.addEventListener('click', operator)
+const equalsButton = document.getElementById('equals-button')
+    equalsButton.addEventListener('click', operator)
+
+function operator(num1, num2) {
+    opSelected = this.textContent
+    result = num1 + opSelected + num2
+    console.log(opSelected)
+}
